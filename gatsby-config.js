7d24d311b922835c80961dbcaeb3d869846ae01b
wadim baslow <wadim.baslow@posteo.de>
2021-02-05 15:43:33 +0100

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,7 +59,7 @@ module.exports = {
       }
     },
     {
-      resolve: gatsby-plugin-google-analytics,
+      resolve: 'gatsby-plugin-google-analytics',
       options: {
         trackingId: process.env.GOOGLE_TAGMANAGER_ID,
         head: true,
