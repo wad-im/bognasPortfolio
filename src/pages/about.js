4fedb4cddb39d84c09ed27cf5bcc5af287625a2f
wadim baslow <wadim.baslow@posteo.de>
2021-03-11 11:56:16 +0100

    split about text into two blocks manually

--- src/pages/about.js
+++ src/pages/about.js
@@ -28,11 +28,13 @@ const AboutPage = ({location})=>{
             <SEO title="About"/>
             <Title>Bogna Anna</Title>
             <AboutGrid>
-                <div className="about_description">
-                    <p>I design with an approach shaped by mixed interests in art and technical disciplines . My education in architecture and sustainability studies are threads that continue to be present in my design work.</p>
-                    <p>While designing I enter a space of flow, where I work fully focused and committed to create the best solution to each unique project context.</p>
-                    <p>If not by the desk, you can most likely find me in one of the local yoga studios, where I teach and practise.</p>
-                    <p>I am also a founder of URBANE films&talks project  - a project discussing documentary films, cities and sustainable urban future.</p>
+                <div className="text-bl text-bl-1">
+                        <p>I design with an approach shaped by mixed interests in art and technical disciplines . My education in architecture and sustainability studies are threads that continue to be present in my design work.</p>
+                        <p>While designing I enter a space of flow, where I work fully focused and committed to create the best solution to each unique project&nbsp;context.</p>
+                </div>
+                <div className="text-bl text-bl-2">
+                        <p>If not by the desk, you can most likely find me in one of the local yoga studios, where I teach and practise.</p>
+                        <p>I am also a founder of URBANE films&talks project  - a project discussing documentary films, cities and sustainable urban future.</p>
                 </div>
                 <ProfileImg image={data.contentfulAsset.gatsbyImageData} alt={data.contentfulAsset.description}/>
             </AboutGrid>
@@ -45,32 +47,26 @@ const AboutGrid = styled.div`
     display: grid;
     grid-template-columns: repeat(3, 1fr);
     grid-column-gap: 4.545%;
-     .about_description {
-         grid-column: 1 / span 2;
-        /* grid-column: span 2;
-        column-count: 2;
-        column-gap: 4.545%; */
-        
-        p {
+     .text-bl p {
             margin-bottom: 1.5vw;
         }
-        
+    .text-bl-1 {
+        grid-column: 1 / span 1;
+    }
+    .text-bl-2 {
+        grid-column: 2 / span 1;
     }
     @media (max-width: 54rem) {
         margin-top: 3rem;
         grid-template-columns: 1fr 1fr;
-        .about_description {
-            /* grid-column: span 1;
-            column-count: 1;
-            column-gap: 0; */
+        .text-bl-2 {
             grid-column: 1 / span 1;
         }
+        
     }
-
     @media (max-width: 36rem) {
         grid-template-columns: 1fr;
         grid-column-gap: 0;
-        grid-row-gap: 3.75rem;
     }
 `
 
@@ -81,9 +77,12 @@ const ProfileImg = styled(GatsbyImage)`
     aspect-ratio: 1 / 1;
     @media (max-width: 54rem) {
         grid-column: 2 / span 1;
+        grid-row: 1 / span 2;
     }
     @media (max-width: 36rem) {
+        margin-top: 3.75rem;
         grid-column: 1 / span 1;
+        grid-row: 3 / span 1;
     }
 `
 
