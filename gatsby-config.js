66d3f86961f5fb8e2be5b2e92bdde1adcc191756
wadim baslow <wadim.baslow@posteo.de>
2021-07-09 13:54:47 +0200

    added sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,7 @@ module.exports = {
     "gatsby-plugin-styled-components",
     "gatsby-plugin-sharp",
     "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
     {
       resolve: `gatsby-plugin-react-helmet-canonical-urls`,
       options: {
@@ -74,6 +75,5 @@ module.exports = {
         anonymize: true,
       }
     },
-    
   ],
 };
